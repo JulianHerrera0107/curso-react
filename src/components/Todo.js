7923f31a8d3a800483b29e5f644186405d540a19
Julian-Herrera <juliandavih@gmail.com>
2024-12-29 16:05:32 -0500

    Primer commit

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+function ListaPendientes() {
+    return (
+        <div className="card">
+            <h2>Titulo1</h2>
+            <div className="actions">
+                <button className="btn">Crear</button>
+                <button className="btn">Eliminar</button>
+            </div>
+        </div>
+    );
+}
+
+export default ListaPendientes;
