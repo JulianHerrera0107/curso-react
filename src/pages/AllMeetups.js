2e609999721b21bd18877254c0d774dc73bf3fdf
Julian-Herrera <juliandavih@gmail.com>
2024-12-30 21:10:16 -0500

    Cambiando la imagen del arreglo DUMMY DATA por otro diferente

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -14,8 +14,8 @@ const DUMMY_DATA = [
       id: 'm2',
       title: 'Este es nuestro segundo encuentro',
       image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Cabo_San_Juan%2C_Colombia.jpg/273px-Cabo_San_Juan%2C_Colombia.jpg',
+      address: 'Cabo San Juan en el Parque Tayrona en Santa Marta.',
       description:
         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
     },
