1d2c10115e11935588c240bcc893376f1191f5e3
Julian-Herrera <juliandavih@gmail.com>
2025-01-04 11:21:42 -0500

    Utilizando el useContext en la página de Favoritos para presentar el contenido mediante el componente MeetupList y un contandor en la barra de navegación de nuestros favoritos

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react';
 import classes from './MainNavigation.module.css'
+import FavoritesContext from '../../store/favorites-context';
+
 
 function MainNavigation() {
-    return(
+    const favoritesCtx = useContext(FavoritesContext);
+
+    return (
         <header className={classes.header}>
             <div className={classes.logo}>Encuentros React</div>
             <nav>
@@ -14,7 +19,10 @@ function MainNavigation() {
                         <Link to='/new-meetup'>Nuevo Encuentro</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>Mis Favoritos</Link>
+                        <Link to='/favorites'>
+                            Mis Favoritos
+                            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,22 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage(){
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let content;
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>Todavía no tienes Favoritos. Empieza añadiendo uno a tu colección</p>
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+
     return(
-        <div>Página de Favoritos</div>
+        <section>
+            <h1>Mis Encuentros favoritos</h1>
+            {content}
+        </section>
     );
 }
 export default FavoritesPage;
